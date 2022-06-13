23ac1becdd8cffea9d4c7dc34e45458a9460ef45
iamrohit <rohit@gmail.com>
2022-06-13 19:13:08 +0530

    user service test passed.

--- chat/test/user_service_test.dart
+++ chat/test/user_service_test.dart
@@ -7,6 +7,7 @@ import 'package:rethinkdb_dart/rethinkdb_dart.dart';
 
 import 'helpers.dart';
 
+/*
 void main() {
   Rethinkdb rethinkdb = Rethinkdb();
   Connection connection;
@@ -35,48 +36,35 @@ void main() {
 
   });
 }
+*/
 
-// import 'dart:developer';
-//
-// import 'package:chat/src/models/user.dart';
-// import 'package:chat/src/services/user/user_service_impl.dart';
-// import 'package:flutter_test/flutter_test.dart';
-// // ignore: import_of_legacy_library_into_null_safe
-// import 'package:rethinkdb_dart/rethinkdb_dart.dart';
-//
-// import 'helpers.dart';
-//
-// void main() {
-//   Rethinkdb rethinkdb = Rethinkdb();
-//   Connection? connection;
-//   UserService? userService;
-//
-//   setUp(() async {
-//     connection = await rethinkdb.connect(host: "127.0.0.1", port: 28015);
-//     await createDb(rethinkdb, connection!);
-//     userService = UserService(connection!, rethinkdb);
-//   });
-//
-//   tearDown(() async {
-//     await cleanDb(rethinkdb, connection!);
-//   });
-//
-//   test('create a new user document in database', () async {
-//     final user = User(
-//       username: 'test',
-//       photoUrl: 'url',
-//       active: true,
-//       lastseen: DateTime.now(),
-//     );
-//
-//     if (userService != null) {
-//       final userWithId = await userService!.connect(user);
-//       expect(userWithId.id, isNotEmpty);
-//     } else {
-//       log("userService is null");
-//     }
-//   });
-// }
+void main() {
+  Rethinkdb rethinkdb = Rethinkdb();
+  Connection? connection;
+  UserService? userService;
+
+  setUp(() async {
+    connection = await rethinkdb.connect(host: "127.0.0.1", port: 28015);
+    await createDb(rethinkdb, connection!);
+    userService = UserService(connection!, rethinkdb);
+  });
+
+  tearDown(() async {
+    await cleanDb(rethinkdb, connection!);
+  });
+
+  test('create a new user document in database', () async {
+    final user = User(
+      username: 'test',
+      photoUrl: 'url',
+      active: true,
+      lastseen: DateTime.now(),
+    );
+
+    final userWithId = await userService!.connect(user);
+    expect(userWithId.id, isNotEmpty);
+  });
+}
 
 
 
