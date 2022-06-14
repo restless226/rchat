f84fcf27d1ef6f7f7e8c773301e04b861c5ea45e
iamrohit <rohit@gmail.com>
2022-06-14 13:01:02 +0530

    implemented tests for receiving messages

--- chat/test/message_service_test.dart
+++ chat/test/message_service_test.dart
@@ -42,10 +42,65 @@ void main() {
       from: _user1.id,
       to: '3456',
       timestamp: DateTime.now(),
-      contents: 'this is a test message sent from user1 to user2',
+      contents: 'this is a test message sent from user1 to 3456',
     );
 
     final result = await _messageService!.send(_message);
     expect(result, true);
   });
+
+  /// test to check whether messages are received successfully or not
+  test('successfully subscribed and received messages', () async {
+      _messageService!.messages(activeUser: _user2).listen(expectAsync1((message) {
+        expect(message.to, _user2.id);
+        expect(message.id, isNotEmpty);
+      }, count: 2)
+    );
+
+    Message _message = Message(
+      from: _user1.id,
+      to: _user2.id,
+      timestamp: DateTime.now(),
+      contents: 'this is a test message sent from user1 to user2',
+    );
+
+    Message _anotherMessage = Message(
+      from: _user1.id,
+      to: _user2.id,
+      timestamp: DateTime.now(),
+      contents: 'this is a another message sent from user1 to user2',
+    );
+
+    await _messageService?.send(_message);
+    await _messageService?.send(_anotherMessage);
+  });
+
+  /// test to check whether new messages are received successfully or not
+  test('successfully subscribe and received new messages', () async {
+    Message _message = Message(
+      from: _user1.id,
+      to: _user2.id,
+      timestamp: DateTime.now(),
+      contents: 'this is a test message sent from user1 to user2',
+    );
+
+    Message _anotherMessage = Message(
+      from: _user1.id,
+      to: _user2.id,
+      timestamp: DateTime.now(),
+      contents: 'this is a another message sent from user1 to user2',
+    );
+
+    await _messageService?.send(_message);
+
+    // subscribing after _anotherMessage is sent
+    await _messageService?.send(_anotherMessage).whenComplete(
+          () => _messageService!.messages(activeUser: _user2).listen(
+              expectAsync1((message) {
+                expect(message.to, _user2.id);
+          }, count: 2),
+        )
+    );
+  });
+
 }
