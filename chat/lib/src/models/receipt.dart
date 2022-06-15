81faf02abca52097c18fb4ef9d076ca56fcdbc0c
iamrohit <rohit@gmail.com>
2022-06-15 09:24:19 +0530

    created receipt model

---
+++ chat/lib/src/models/receipt.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/foundation.dart';
+
+enum ReceiptStatus {
+  sent,
+  delivered,
+  read,
+}
+
+extension EnumParsing on ReceiptStatus {
+  String value() {
+    return this.toString().split('.').last;
+  }
+
+  static ReceiptStatus fromString(String status) {
+    return ReceiptStatus.values
+        .firstWhere((element) => element.value() == status);
+  }
+}
+
+class Receipt {
+  final String? recipient;
+  final String? messageId;
+  final ReceiptStatus? status;
+  final DateTime? timestamp;
+  String? _receiptId;
+
+  String? get id => _receiptId;
+
+  Receipt({
+    @required this.recipient,
+    @required this.messageId,
+    @required this.status,
+    @required this.timestamp,
+  });
+
+  Map<String, dynamic> toJson() => {
+    'recipient': this.recipient,
+    'messageId': this.messageId,
+    'status': this.status,
+    'timestamp': this.timestamp,
+  };
+
+  factory Receipt.fromJson(Map<String, dynamic> json) {
+    Receipt receipt = Receipt(
+      recipient: json['recipient'],
+      messageId: json['messageId'],
+      status: json['status'],
+      timestamp: json['timestamp'],
+    );
+    receipt._receiptId = json['id'];
+    return receipt;
+  }
+}
