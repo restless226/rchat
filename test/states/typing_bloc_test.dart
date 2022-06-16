16dcbc6636408666f0dfa2553cf30022179fc860
iamrohit <rohit@gmail.com>
2022-06-16 15:59:13 +0530

    failed to implement typing bloc test due to mockito

---
+++ test/states/typing_bloc_test.dart
@@ -0,0 +1,63 @@
+// import 'package:chat/chat.dart';
+// import 'package:my_chat_app/states_management/typing/typing_notification_bloc.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:mockito/mockito.dart';
+//
+// class FakeTypingNotificationService extends Mock
+//     implements ITypingNotification {}
+//
+// void main() {
+//   TypingNotificationBloc sut;
+//   ITypingNotification typingNotificationService;
+//   User user;
+//
+//   setUp(() {
+//     typingNotificationService = FakeTypingNotificationService();
+//     user = User(
+//       username: 'test',
+//       photoUrl: '',
+//       active: true,
+//       lastseen: DateTime.now(),
+//     );
+//     sut = TypingNotificationBloc(typingNotificationService);
+//   });
+//
+//   tearDown(() => sut.close());
+//
+//   test('should emit initial state only without subscriptions', () {
+//     expect(sut.state, TypingNotificationInitial());
+//   });
+//
+//   test('should emit typing sent state when typing event is sent', () {
+//     final typingEvent =
+//         TypingEvent(chatId: '123', from: '111', to: '222', event: Typing.start);
+//     when(typingNotificationService.send(events: [typingEvent]))
+//         .thenAnswer((_) async => true);
+//     sut.add(TypingNotificationEvent.onTypingEventSent([typingEvent]));
+//     expectLater(sut.stream, emits(TypingNotificationState.sent()));
+//   });
+//
+//   test('should not subscribe if no chats exist', () {
+//     final typingEvent =
+//         TypingEvent(chatId: '123', from: '111', to: '222', event: Typing.start);
+//
+//     when(typingNotificationService.subscribe(any, any))
+//         .thenAnswer((_) => Stream.fromIterable([typingEvent]));
+//
+//     sut.add(TypingNotificationEvent.onSubscribed(user));
+//     verifyNever(typingNotificationService.subscribe(any, any));
+//     expectLater(sut.stream, emits(TypingNotificationState.initial()));
+//   });
+//   test('should receive typing events from service', () {
+//     final typingEvent =
+//         TypingEvent(chatId: '123', from: '111', to: '222', event: Typing.start);
+//
+//     when(typingNotificationService.subscribe(any, any))
+//         .thenAnswer((_) => Stream.fromIterable([typingEvent]));
+//
+//     sut.add(TypingNotificationEvent.onSubscribed(user, usersWithChat: ['123']));
+//     verifyNever(typingNotificationService.subscribe(any, any));
+//     expectLater(sut.stream,
+//         emitsInOrder([TypingNotificationReceivedSuccess(typingEvent)]));
+//   });
+// }
