cef906748a68848dae8f6ebcdeee2bd28cd6bee9
iamrohit <rohit@gmail.com>
2022-06-17 11:11:35 +0530

    added screenshots, plugins, colors, theme files

---
+++ lib/theme.dart
@@ -0,0 +1,48 @@
+import 'package:my_chat_app/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const appBarTheme = AppBarTheme(
+  centerTitle: false,
+  elevation: 0,
+  backgroundColor: Colors.white,
+);
+
+final tabBarTheme = TabBarTheme(
+  indicatorSize: TabBarIndicatorSize.label,
+  unselectedLabelColor: Colors.black54,
+  indicator: BoxDecoration(
+    borderRadius: BorderRadius.circular(50),
+    color: kPrimary,
+  ),
+);
+
+final dividerTheme =
+    const DividerThemeData().copyWith(thickness: 1.2, indent: 75.0);
+
+ThemeData lightTheme(BuildContext context) => ThemeData.light().copyWith(
+      primaryColor: kPrimary,
+      scaffoldBackgroundColor: Colors.white,
+      appBarTheme: appBarTheme,
+      tabBarTheme: tabBarTheme,
+      dividerTheme: dividerTheme,
+      iconTheme: const IconThemeData(color: kIconLight),
+      textTheme: GoogleFonts.comfortaaTextTheme(Theme.of(context).textTheme)
+          .apply(displayColor: Colors.black),
+      visualDensity: VisualDensity.adaptivePlatformDensity,
+    );
+
+ThemeData darkTheme(BuildContext context) => ThemeData.dark().copyWith(
+    primaryColor: kPrimary,
+    scaffoldBackgroundColor: Colors.black,
+    tabBarTheme: tabBarTheme.copyWith(unselectedLabelColor: Colors.white70),
+    dividerTheme: dividerTheme.copyWith(color: kBubbleDark),
+    appBarTheme: appBarTheme.copyWith(backgroundColor: kAppBarDark),
+    iconTheme: const IconThemeData(color: Colors.black),
+    textTheme: GoogleFonts.comfortaaTextTheme(Theme.of(context).textTheme)
+        .apply(displayColor: Colors.white),
+    visualDensity: VisualDensity.adaptivePlatformDensity);
+
+bool isLightTheme(BuildContext context) {
+  return MediaQuery.of(context).platformBrightness == Brightness.light;
+}
